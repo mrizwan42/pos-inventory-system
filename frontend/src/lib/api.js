fa885699de601cab93923789fd51011a76ddba51
mrizwan42 <77779307+mrizwan42@users.noreply.github.com>
2025-08-02 21:50:18 +0500

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.PROD 
-    ? 'http://your-username.pythonanywhere.com' // <-- HARDCODED PRODUCTION URL
+    ? 'http://rizwanm42.pythonanywhere.com' // <-- HARDCODED PRODUCTION URL
     : 'http://localhost:5000', // Fallback for local development
 } );
 
